444805f5720ea33b9070deec83a860ef16e1202e
Bouchoucha Jordan <jordan.bouchoucha@etudiant.univ-lille1.fr>
2017-11-22 16:49:01 +0100

    plot tp1 & k var

--- tp1.py
+++ tp1.py
@@ -7,6 +7,7 @@ Ceci est un script temporaire.
 
 from sklearn import datasets
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Récupération des données
 
@@ -18,22 +19,35 @@ n          = len(data['data'])
 nb_classes = len(data['target_names'])
 nb_attr    = len(data['feature_names'])
 
+                       
+# Fonction de génération des ensembles
+def generate_sets():
+    x = [data['data'][0:50], data['data'][50:100], data['data'][100:150]]
+    y = [data['target'][0:50], data['target'][50:100], data['target'][100:150]]
+    X_app = []
+    X_test = []
+    Y_app = []
+    Y_test = []
+    
+    # On prend 25 données dans les 3 classes
+    for i in range(3):
+        indexs = [w for w in range(50)]
+        np.random.shuffle(indexs)
+
+        for j in range(len(x[i])):
+            if j < 25:
+                X_app.append(x[i][indexs[j]])    
+                Y_app.append(y[i][indexs[j]])
+            else:
+                X_test.append(x[i][indexs[j]])
+                Y_test.append(y[i][indexs[j]])
+    
+    return X_app, Y_app, X_test, Y_test
+
 # Création des deux ensembles d'apprentissage et 
 # de test
 
-D_app  = np.concatenate((data['data'][0:25],
-                        data['data'][50:75],
-                        data['data'][100:125]))
-D_test = np.concatenate((data['data'][25:50],
-                        data['data'][75:100],
-                        data['data'][125:150]))
-
-Y_app  = np.concatenate((data['target'][0:25],
-                        data['target'][50:75],
-                        data['target'][100:125]))
-Y_test = np.concatenate((data['target'][25:50],
-                        data['target'][75:100],
-                        data['target'][125:150]))
+X_app, Y_app, X_test, Y_test = generate_sets()
 
 # Permet de renvoyer la distance entre deux exemples
 def compute_distance(x, y):
@@ -44,25 +58,35 @@ def compute_distance(x, y):
     
     
 # Algorithme des k plus proches voisins
-def kppv(x, app, k):
+def kppv(x, app, yapp, k):
     # On regarde les k plus proche voisins de x,
     # selon ses 4 attributs
     distances = []
+    
     for i in range(len(app)):
         distances.append(compute_distance(x, app[i]))
 
     sort_index_distances = np.argsort(distances)
 
-    # On regarde les classes de ces 4 attributs et
-    # on prend la plus dominante
+    # On crée le tableau des classes "les plus proches"
+    # et on décide ainsi de laquelle est la plus proche
     neighbours_classes = []
     for i in range(k):
-        neighbours_classes.append(Y_app[sort_index_distances[i]])
+        neighbours_classes.append(yapp[sort_index_distances[i]])
 
     repartition = np.bincount(neighbours_classes)
     prediction = np.argmax(repartition)
-    return prediction
 
-    # Si il y a une égalité, on fait la diff
-    # entre les sommes
-    #if np.count_nonzero(repartition == prediction) > 1:
+    return prediction
+        
+kv = []
+for k in range(1, 75):
+    t = 0
+    for i in range(len(X_test)):
+        if(kppv(X_test[i], X_app, Y_app, k) == Y_test[i]):
+            t += 1
+    print("k: ", end='')
+    print(k, end=' ')
+    print(t/75)
+
+plt.plot(kv)
